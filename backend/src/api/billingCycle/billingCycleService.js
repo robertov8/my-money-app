c098891f9b63248837af24f2c00c109ea2aff3b9
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-23 16:03:05 -0300

    Section 8, class 90

---
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,6 @@
+require('./billingCycle');
+
+BillingCycle.methods(['get', 'post', 'put', 'delete']);
+BillingCycle.updateOptions({ new: true, runValidators: true });
+
+module.exports = BillingCycle;
