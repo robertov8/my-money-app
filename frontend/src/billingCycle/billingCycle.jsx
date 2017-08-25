9b6eaf0474d3017affa27190700c47d4e54a3026
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-25 20:38:28 -0300

    Section 9, class 121

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default props => (
-    <div>
-        <h1>Ciclo de Pagamentos</h1>
-    </div>
-)
+import ContentHeader from '../common/template/contentHeader';
+import Content from '../common/template/content';
+
+class BillingCycle extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
+                <Content>
+                    <h1>Ciclo de Pagamentos</h1>
+                </Content>
+            </div>
+        );
+    }
+}
+
+export default BillingCycle
