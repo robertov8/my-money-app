1b78b1636f63f148b0af7af95930bad5863e1dc5
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-26 02:21:39 -0300

    Section 9, class 134

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -10,8 +10,18 @@ class BillingCycleList extends Component {
         this.props.getList();
     }
 
+    renderRows() {
+        const list = this.props.list || [];
+        return list.map(bc => (
+            <tr key={bc._id}>
+                <td>{bc.name}</td>
+                <td>{bc.month}</td>
+                <td>{bc.year}</td>
+            </tr>
+        ));
+    }
+
     render() {
-        console.log(this.props.list);
         return (
             <div>
                 <table className='table'>
@@ -24,7 +34,7 @@ class BillingCycleList extends Component {
                     </thead>
 
                     <tbody>
-
+                        {this.renderRows()}
                     </tbody>
                 </table>
             </div>
