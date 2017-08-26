e09e3d04383621a51838172abb7c0fa3b9b0e1c7
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-25 21:34:57 -0300

    Section 9, class 126

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -6,6 +6,7 @@ import Tabs from '../common/tab/tabs';
 import TabsHeader from '../common/tab/tabsHeader';
 import TabsContent from '../common/tab/tabsContent';
 import TabHeader from '../common/tab/tabHeader';
+import TabContent from '../common/tab/tabContent';
 
 class BillingCycle extends Component {
     render() {
@@ -15,14 +16,17 @@ class BillingCycle extends Component {
                 <Content>
                     <Tabs>
                         <TabsHeader>
-                            <TabHeader label='Listar' icon='bars' target='tabListar' />
+                            <TabHeader label='Listar' icon='bars' target='tabList' />
                             <TabHeader label='Incluir' icon='plus' target='tabCreate' />
                             <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
                             <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
                         </TabsHeader>
 
                         <TabsContent>
-
+                            <TabContent id='tabList'><h1>Listar</h1></TabContent>
+                            <TabContent id='tabCreate'><h1>Create</h1></TabContent>
+                            <TabContent id='tabUpdate'><h1>Update</h1></TabContent>
+                            <TabContent id='tabDelete'><h1>Delete</h1></TabContent>
                         </TabsContent>
                     </Tabs>
                 </Content>
