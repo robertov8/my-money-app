2170a64542fec653f78704147355a45bfadcf88d
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-26 15:27:26 -0300

    Section 9, class 137

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import { reducer as toastrReducer } from 'react-redux-toastr';
 
 import DashboardReducer from '../dashboard/dashboardReducers';
 import TabReducer from '../common/tab/tabReducers';
@@ -9,7 +10,8 @@ const rootReducer = combineReducers({
     dashboard: DashboardReducer,
     tab: TabReducer,
     billingCycle: BillingCycleReducers,
-    form: formReducer
+    form: formReducer,
+    toastr: toastrReducer
 });
 
 export default rootReducer;
