507982598d949cac5e210048c0ed88376fb20749
Roberto Ribeiro <robertov82008@gmail.com>
2017-08-27 23:39:27 -0300

    Section 12, class 167

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -5,13 +5,15 @@ import { reducer as toastrReducer } from 'react-redux-toastr';
 import DashboardReducer from '../dashboard/dashboardReducers';
 import TabReducer from '../common/tab/tabReducers';
 import BillingCycleReducers from '../billingCycle/billingCyleReducers';
+import AuthReducer from '../auth/authReducer';
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
     tab: TabReducer,
     billingCycle: BillingCycleReducers,
     form: formReducer,
-    toastr: toastrReducer
+    toastr: toastrReducer,
+    auth: AuthReducer
 });
 
 export default rootReducer;
